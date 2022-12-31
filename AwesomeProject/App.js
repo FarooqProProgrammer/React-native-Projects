d94fb2fa72fc74d94a0f93e34b86d07be0e9ce38
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-31 12:01:03 +0500

    updated

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -2,11 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 const Tab = createMaterialTopTabNavigator();
+function HomeScreen(){
+  return(
+    <View></View>
+  )
+}
+function SettingsScreen(){
+  return(
+    <View></View>
+  )
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MyTabs/>
     </View>
   );
 }
