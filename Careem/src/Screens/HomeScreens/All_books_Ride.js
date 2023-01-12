ff8587bfd82b2fe10dee77520c67bed5a2e61c84
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-01-12 18:13:32 +0500

    updated

--- Careem/src/Screens/HomeScreens/All_books_Ride.js
+++ Careem/src/Screens/HomeScreens/All_books_Ride.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 
 export default function AllRides(){
   
+    const [distance,setDistance] = useState();
     const Ride = useSelector(state => state)
     console.log(Ride.location);
     
@@ -32,6 +33,7 @@ export default function AllRides(){
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
         var d = R * c; // Distance in km
         console.log(d);
+        setDistance(d)
       }
       
 
@@ -39,7 +41,29 @@ export default function AllRides(){
     return(
         <View style={styles.container}>
             <View style={styles.Box}>
-                <Button onPress={()=> getDistanceFromLatLonInKm(Ride.location.latitude, Ride.location.longitude, Destination.location.latitude, Destination.location.longitude)} title="Fare"></Button>
+                
+                <View style={styles.left}>
+                   
+                 
+                    <Text style={styles.Text}>Distance: 10km </Text>
+                    <Text style={styles.Text}>Fare: 20 x 1km </Text>
+                    <Text style={styles.Text}>Total Fare: 200Rs </Text>
+                   
+                  
+                    
+                </View>
+                <View style={styles.Right}>
+                    <Button onPress={()=> getDistanceFromLatLonInKm(Ride.location.latitude, Ride.location.longitude, Destination.location.latitude, Destination.location.longitude)} title="Fare"></Button>
+                </View>
+                
+            </View>
+            <View style={styles.Car}>
+                    <View>
+                        <Text style={styles.Text}> Select Your Desired Car</Text>
+                    </View>
+                    <View style={styles.AllCars}>
+
+                    </View>
             </View>
         </View>
     )
@@ -48,15 +72,46 @@ export default function AllRides(){
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        
-        justifyContent:"center",
-        alignItems:"center"
     },
     Box:{
         width:350,
-        height:80,
+        height:200,
         borderWidth:2,
         borderRadius:4,
+        borderColor:"black",
+        flexDirection:"row",
+        justifyContent:"center",
+        alignItems:"center"
+    },left:{
+        flex:2,
+        borderWidth:2,
+        height:200,
+        borderColor:"black",
+        justifyContent:"space-around",
+        alignItems:"center"
+    },Right:{
+        flex:1,
+        borderWidth:2,
+        height:200,
+        borderColor:"black",
+        justifyContent:"center",
+        alignItems:"center"
+    },Text:{
+        fontSize:20,
+        fontWeight:"500"
+    },Car:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:'center',
+        height:200,
+        borderWidth:4,
+        borderColor:"black",
+        overflow:"scroll"
+    },AllCars:{
+        flex:1,
+        width:"100%",
+        borderWidth:2,
         borderColor:"black"
     }
+
 })
